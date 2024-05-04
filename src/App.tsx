eac3bac12828c46cf4f1e1fb282e7301991fa777
simply_ark0 <arkojeetbera@simply-arko.local>
2024-05-04 14:42:09 +0530

    UI Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
-import { FaPlus } from "react-icons/fa6";
 import TodoCard from "./components/TodoCard";
+import DialogInput from "./components/DialogInput";
 
 export default function App() {
     useEffect(() => {
@@ -14,10 +13,7 @@ export default function App() {
         <div className="text-white h-full max-w-[1200px] px-[200px] pt-[80px] mx-auto">
             <div className="flex flex-row justify-between">
                 <h1 className="text-2xl font-semibold">Memo-Rise</h1>
-                <Button className="bg-[#3275F6]">
-                    <FaPlus className="mr-2" />
-                    Add a new note
-                </Button>
+                <DialogInput />
             </div>
             <div className="mt-4 p-4">
                 <TodoCard />
---
+++ src/components/DialogInput.tsx
@@ -0,0 +1,76 @@
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { FaPlus } from "react-icons/fa6";
+import { Textarea } from "./ui/textarea";
+
+export default function DialogInput() {
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <Button className="bg-[#3275F6]">
+                    <FaPlus className="mr-2" />
+                    Add a new note
+                </Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[600px] text-white">
+                <DialogHeader>
+                    <DialogTitle className="text-center text-2xl">
+                        Add a new note
+                    </DialogTitle>
+                </DialogHeader>
+                <div className="grid gap-4 py-4">
+                    <div className="grid grid-cols-6 items-center gap-4">
+                        <Label
+                            htmlFor="title"
+                            className="text-right text-lg font-semibold"
+                        >
+                            Title:
+                        </Label>
+                        <Input
+                            id="title"
+                            placeholder="Title"
+                            className="col-span-5"
+                        />
+                    </div>
+                    <div className="grid grid-cols-6 items-center gap-4">
+                        <Label
+                            htmlFor="tags"
+                            className="text-right text-lg font-semibold"
+                        >
+                            Tags:
+                        </Label>
+                        <Input
+                            id="tags"
+                            placeholder="space-separated"
+                            className="col-span-5"
+                        />
+                    </div>
+                    <div className="grid grid-cols-6 items-center gap-4">
+                        <Label
+                            htmlFor="content"
+                            className="text-right text-lg font-semibold self-start"
+                        >
+                            Note:
+                        </Label>
+                        <Textarea
+                            id="content"
+                            className="col-span-5 min-h-[100px] dark:bg-slate-950"
+                        />
+                    </div>
+                </div>
+                <DialogFooter>
+                    <Button type="submit">Save</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    );
+}
